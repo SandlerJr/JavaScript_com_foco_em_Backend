7d47db413c32752843aa9e80227a217fdced54f3
SandlerJr <sandlerjunin@gmail.com>
2023-11-17 16:12:11 -0300

    melhorando e limpando codigo de expressoes-funcao.js.

--- Fundamentos-js-master/expressoes-funcao.js
+++ Fundamentos-js-master/expressoes-funcao.js
@@ -1,7 +1,8 @@
 // declaração de função
 
 function minhaFuncao(param) {
-  // bloco de código
+  const mensagem = param;
+  console.log(mensagem);
 }
 
 minhaFuncao("param")
@@ -14,11 +15,13 @@ console.log(soma(1, 1))
 // diferença principal: HOISTING
 // funções e var são "listadas" no topo do arquivo.
 
-console.log(apresentar())
+// chama a função antes de declara-la
+console.log(apresentar());
 
 function apresentar() {
   return "olá";
 }
 
+// nao pode ser chamada antes de declara-la(vai dar ReferenceError - Proposital)
 console.log(somaOutroExemplo(1, 1))
 const somaOutroExemplo = function(num1, num2) { return num1 + num2 }
