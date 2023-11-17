4f60a7d28f1f3457f85cbac5cec2ceec8772d9eb
SandlerJr <sandlerjunin@gmail.com>
2023-11-17 15:10:04 -0300

    Limpando codigo da pasta console.js

--- Fundamentos-js-master/comparacoes.js
+++ Fundamentos-js-master/comparacoes.js
@@ -1,15 +1,12 @@
-// == comparação implícita
+// == comparação
 
 const numero = 5;
 const texto = "5";
 
 const doisIguais = numero == texto;
-
-// === comparação explicita
 const tresIguais = numero === texto;
 
 console.log([doisIguais, tresIguais]);
-
 
 // == só compara o valor
 // === compara o valor e o tipo de dado
