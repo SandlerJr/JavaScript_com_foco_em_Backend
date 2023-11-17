29ad6e48d341554609aa9a7536786dddbc1734c0
SandlerJr <sandlerjunin@gmail.com>
2023-11-17 13:43:37 -0300

    melhorando codigo

--- Fundamentos-js-master/truthy-falsy.js
+++ Fundamentos-js-master/truthy-falsy.js
@@ -1,36 +1,47 @@
-const x = 0;
+/*Boolean
+    true
+    false
+*/
 
-//Boolean
-const usuarioLogado = true
-const contaPaga = false
+function imprimindoVerdadeiroEFalse(){
+    const usuarioLogado = true;
+    const contaPaga = false;
+
+    const mensagem =[usuarioLogado, contaPaga];
+    console.log(mensagem);
+}
 
 //truthy ou falsy
 //0 -> false
 //1 -> true
 
-console.log(0 == false)//retorna true
-console.log("" == false)//retorna true
-console.log(1 == true)
+function imprimindoVerdadeiroEFalse2(){
+    console.log(0 == false);//retorna true
+    console.log("" == false);//retorna true
+    console.log(1 == true);
+}
 
 
-console.log("null e undefinned")
-//undefined
-//null -> vazio ou nada
+function vendoTipos(){
+    let numero = 3;
+    let texto = "Alura";
+    let minhaVar; // Não tem valor definido
+    let varNull = null; // valor igual a nada/vazio/nulo 
 
-let minhaVar;
-let varNull = null;
+    //vendo valores
+    console.log(minhaVar);
+    console.log(varNull);
 
-console.log(minhaVar);
-console.log(varNull);
-
-console.log("Quando o undefined está em sua forma pura")
-
-let numero = 3;
-let texto = "Alura";
-
-console.log(typeof numero);
-console.log(typeof texto);
+    //vendo tipos
+    console.log(typeof numero);
+    console.log(typeof texto);
 
 
-console.log(typeof minhaVar);
-console.log(typeof varNull);
+    console.log(typeof minhaVar);
+    console.log(typeof varNull);
+
+}
+
+imprimindoVerdadeiroEFalse();
+imprimindoVerdadeiroEFalse2();
+tipos();
