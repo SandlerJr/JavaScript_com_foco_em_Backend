963ec72ab0a51f3d3806729aae7abefd6347d1a7
SandlerJr <sandlerjunin@gmail.com>
2023-11-17 14:33:43 -0300

    Melhorando o codigo do file conversoes.js

--- Fundamentos-js-master/conversoes.js
+++ Fundamentos-js-master/conversoes.js
@@ -1,14 +1,18 @@
-// tipo de dado 
-// booleanos 
-
 // conversão implícita
-const numero = 456;
-const numeroString = Number("456a");
 
-//Number()
-//String()
-console.log(numero + numeroString)
+const numero = 456;
+const StringParaNumero = String(numero)
+
+console.log(StringParaNumero);
+
+//string para numero de outra forma
+const numeroString = Number("456");
+console.log(numero + numeroString); // vai conseguir fazer a conta
+
+
+//letras e numeros na string a ser convertida 
+const numeroStringError = Number("456a"); 
+
+console.log(numero + numeroStringError); // resultsado sera: NaN
 
 // conversão explícita
-
-console.warn(new Error('oi'))
